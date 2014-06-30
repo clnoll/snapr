68f9fa16936c8cf85ea4a94866ba45d02d63884b
clnoll <noll.catherine@gmail.com>
2014-06-29 18:40:51 -0700

    specs and code for user orm queries

--- spec/orm_spec.rb
+++ spec/orm_spec.rb
@@ -8,8 +8,8 @@ describe "ORM" do
   end
 
   before(:each) do
-    RPS.orm.delete_tables
-    RPS.orm.add_tables
+    Snapr.orm.delete_tables
+    Snapr.orm.add_tables
   end
 
   it "should be an ORM" do
@@ -20,16 +20,46 @@ describe "ORM" do
     expect(Snapr.orm.db_adaptor).not_to be_nil
   end
 
-
-  describe "user_login" do
-    it ""
-
+  describe "create_user" do
+    it "creates a user as a User object" do
+      user1 = Snapr.orm.create_user('first_user', 'pw')
+      user2 = Snapr.orm.create_user('second_user', 'pw')
+      expect(user1).to be_a(Snapr::User)
+      expect(user1.username).to eq('first_user')
+      expect(user1.id).to be_a(Fixnum)
+      expect(user2.username).to eq('second_user')
+      expect(user2.id).to_not eq(user1.id)
+    end
   end
 
-  describe "user_signup" do
+  describe "get_user" do
+    it "initializes the user as a User object if the user exists" do
+      user1 = Snapr.orm.create_user('first_user', 'pw')
+      user = Snapr.orm.get_user(user1.username)
+      expect(user).to be_a(Snapr::User)
+      expect(user.id).to eq(user1.id)
+      expect(user.username).to eq(user1.username)
+      expect(user.password).to eq(user1.password)
+    end
+
+    it "returns nil if the user doesn't exist" do
+      user = Snapr.orm.get_user(22)
+      expect(user).to be_nil
+    end
   end
 
   describe "create_profile" do
+    it "updates the user's profile" do
+      user1 = Snapr.orm.create_user('first_user', 'pw')
+      user = Snapr.orm.create_profile(user1.id, 88, 'Oakland', 'CA', 'male', 'women', 'hi there')
+      expect(user).to be_a(Snapr::User)
+      expect(user.age).to eq('88')
+      expect(user.city).to eq('Oakland')
+      expect(user.state).to eq('CA')
+      expect(user.gender).to eq('male')
+      expect(user.gender_pref).to eq('women')
+      expect(user.description).to eq('hi there')
+    end
   end
 
   # describe "update_image" do
--- spec/spec-helper.rb
+++
@@ -1,7 +0,0 @@
-require 'pry-debugger'
-# Require our project, which in turns requires everything else
-require_relative '../lib/snapr.rb'
-
-RSpec.configure do |config|
-  RPS.orm.instance_variable_set(:@db_adaptor, PG.connect(host: 'localhost', dbname: 'snapr-test') )
-end
