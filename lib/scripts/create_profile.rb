3c5bc912d1b849ba1fd486f2626989bdfd8a11f8
clnoll <noll.catherine@gmail.com>
2014-06-30 11:22:38 -0700

    updates to transaction script for user signup

---
+++ lib/scripts/create_profile.rb
@@ -0,0 +1,26 @@
+class Snapr::CreateProfile
+
+  def self.run(inputs)
+    self.new.run(inputs)
+  end
+
+  def run(input)
+
+    # user = Snapcat::Client.new(input[:username])
+    # if user.nil?
+    #   return { :success? => false, :error => 'Get snapchat login'}
+    # end
+    # user.login(input[:password])
+
+    # username = Snapr.orm.get_user(input[:username])
+    # if username.nil?
+    #   username = Snapr.orm.create_user(input[:username], input[:password])
+    #   return { :success? => true, :username => username }
+    # end
+
+    # { :success? => false, :error => "Username is taken" }
+
+    # end
+  end
+
+end
