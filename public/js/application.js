f51fc26660b8b62ea96063b7a78ed5d3d63a3c2f
alex haines <hainesaxiv@gmail.com>
2014-07-01 11:33:11 -0700

    added login controller

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,77 @@
-'use strict';
+  // create the module and name it scotchApp
+  angular.module('Snapr', ['ngRoute', 'ngResource'])
 
+  // .config(function($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       controller: 'Ctrl',
+  //       templateUrl: 'partials/blah.html'
+  //     })
+  // })
+  // .controller('Ctrl', function() {
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  // })
+
+  // configure our routes
+  .config(function($routeProvider) {
+    $routeProvider
+
+      // route for the login page
+      .when('/', {
+        templateUrl : 'partials/blah.html',
+        controller  : 'mainController'
+      })
+
+      // route for the user page
+      .when('/login', {
+        templateUrl : 'partials/login.html',
+        controller  : 'loginController'
+      })
+
+  //     // route for the profile page
+      .when('/user/:id', {
+        templateUrl : 'partials/potential_matches.html',
+        controller  : 'mainController'
+      })
+
+      .when('/user/:id/matches', {
+        templateUrl : 'partials/matches.html',
+        controller  : 'mainController'
+      })
+
+      .when('/signup', {
+        templateUrl : 'partials/signup.html',
+        controller  : 'mainController'
+      });
+  })
+
+  // create the controller and inject Angular's $scope
+  .controller('mainController', function($scope, $http) {
+    // create a message to display in our view
+    // $scope.login = function() {
+    //   $scope.login_result = $http.post('/').success(function(data){
+    //     $scope.login_result = data;
+    //   });
+  })
+
+  .controller('loginController', function($scope, $http) {
+    // create a message to display in our view
+    $scope.user = {username: 'user', password: 'pw'};
+
+    $scope.login = function() {
+      $scope.login_result = $http.post('/login', {user: $scope.user}, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).success(function(data){
+        alert('hi');
+        // $timeout(function() {
+        //   $location.path('/user/:id');
+        // })
+      })
+    }
+  })
+
+  // snapr.controller('aboutController', function($scope, , $http) {
+  //   $scope.message = 'Look! I am an about page.';
+  // });
+
+  // snapr.controller('contactController', function($scope, , $http) {
+  //   $scope.message = 'Contact us! JK. This is just a demo.';
+  // });
--- server.rb
+++ server.rb
@@ -3,77 +3,88 @@ require "sinatra/json"
 require "sinatra/reloader" if development?
 require_relative 'lib/snapr.rb'
 require 'pry-debugger'
+require 'json'
 # require 'rack/test'
 enable :sessions
 set :bind, '0.0.0.0'
 
 
 get '/' do
-  erb :login
+  send_file 'index.html'
 end
 
-post '/' do
-  result = Snapr::UserLogin.run({username: params[:username]})
+post '/login' do
+  puts params
+  params = JSON.parse(request.body.read.to_s)
+  login_info = params['user']
+  login_result = Snapr::UserLogin.run(login_info)
+  json login_result.to_json
 
-  if result[:success?]
-    session[:id] = result[:id]
-    redirect to("/#{session[:id]}")
+  if login_result[:success?]
+    session[:id] = login_result[:id]
   else
-    @error = result[:error]
-    redirect to('/login')
-  end
-end
-
-get '/signup' do
-  erb :signup
-end
-
-post '/signup' do
-  if result[:success?]
-    session[:id] = result[:id]
-  else
-    @error = result[:error]
-    redirect to('/signup')
-  end
-end
-
-post ':id/profile' do
-  if session[:id]
-    edit_profile = Snapr::CreateProfile.run({id: session[:id], age: params[:age], city: params[:city], state: params[:state], gender: params[:gender], gender_pref: params[:gender_pref], description: params[:description]})
-    redirect to("/login")
-  else
-    redirect to('/login')
-  end
-end
-
-get ':id/profile' do
-  if session[:id]
-    erb :profile
-  else
-    redirect to('/login')
-  end
-end
-
-get '/:id/matches' do
-  if session[:id]
-    show_matches = RPS::ViewMatches.run({id: session[:id]})
-    @match_results = show_matches[:profiles]
-    erb :matches
-  else
-    redirect to('/login')
-  end
-end
-
-get '/:id' do
-  if session[:id]
-    erb :potential_matches
-  else
-    redirect to('/login')
+    @error = login_result[:error]
   end
 
-  user = Snapr.orm.get_user(session[:id])
-
-  result = Snapr::ViewProfiles(session[:id], user.gender_pref)
-
-  @profiles = result[:profiles]
+  json ["hey"]
 end
+
+
+# get 'users/signup' do
+#   erb :signup
+# end
+
+# post 'users/signup' do
+#   if result[:success?]
+#     session[:id] = result[:id]
+#   else
+#     @error = result[:error]
+#     redirect to('/signup')
+#   end
+# end
+
+# post 'users/:id/profile' do
+#   if session[:id]
+#     edit_profile = Snapr::CreateProfile.run({id: session[:id], age: params[:age], city: params[:city], state: params[:state], gender: params[:gender], gender_pref: params[:gender_pref], description: params[:description]})
+#     redirect to("/login")
+#   else
+#     redirect to('/login')
+#   end
+# end
+
+# get 'users/:id/profile' do
+#   if session[:id]
+#     erb :profile
+#   else
+#     redirect to('/login')
+#   end
+# end
+
+# get 'users/:id/matches' do
+#   if session[:id]
+#     show_matches = RPS::ViewMatches.run({id: params[:id]})
+#     @match_results = show_matches[:profiles]
+#     @matches.map! { |user| json user.to_json }
+#     erb :matches
+#   else
+#     redirect to('/login')
+#   end
+# end
+
+# get 'users/:id' do
+#   if session[:id]
+#     erb :potential_matches
+#   else
+#     redirect to('/login')
+#   end
+
+#   user = Snapr.orm.get_user(session[:id])
+
+#   result = Snapr::ViewProfiles({id: params[:id], g_pref: user.gender_pref})
+
+#   @profiles = result[:profiles]
+
+#   @profiles.map! { |user| json user.to_json }
+
+#   @profiles
+# end
