428484eb2fe2589693415713efdfdc1730d2fdd8
clnoll <noll.catherine@gmail.com>
2014-07-03 11:40:11 -0700

    merge with cnoll

---
+++ public/js/getimage.js
@@ -0,0 +1,58 @@
+// (function() {
+// $('#startbutton').click(function(){console.log('hi');});
+//   var streaming = false,
+//       video        = document.querySelector('#video'),
+//       canvas       = document.querySelector('#canvas'),
+//       photo        = document.querySelector('#photo'),
+//       startbutton  = document.querySelector('#startbutton'),
+//       width = 320,
+//       height = 0;
+
+//   navigator.getMedia = ( navigator.getUserMedia ||
+//                          navigator.webkitGetUserMedia ||
+//                          navigator.mozGetUserMedia ||
+//                          navigator.msGetUserMedia);
+
+//   navigator.getMedia(
+//     {
+//       video: true,
+//       audio: false
+//     },
+//     function(stream) {
+//       if (navigator.mozGetUserMedia) {
+//         video.mozSrcObject = stream;
+//       } else {
+//         var vendorURL = window.URL || window.webkitURL;
+//         video.src = vendorURL.createObjectURL(stream);
+//       }
+//       video.play();
+//     },
+//     function(err) {
+//       console.log("An error occured! " + err);
+//     }
+//   );
+
+//   video.addEventListener('canplay', function(ev){
+//     if (!streaming) {
+//       height = video.videoHeight / (video.videoWidth/width);
+//       video.setAttribute('width', width);
+//       video.setAttribute('height', height);
+//       canvas.setAttribute('width', width);
+//       canvas.setAttribute('height', height);
+//       streaming = true;
+//     }
+//   }, false);
+
+//   var takepicture = function() {
+//     canvas.width = width;
+//     canvas.height = height;
+//     var data = canvas.toDataURL('image/png');
+//     photo.setAttribute('src', data);
+//     canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+//   };
+
+//   startbutton.addEventListener('click', function(ev){
+//       takepicture();
+//     ev.preventDefault();
+//   }, false);
+// })();
